e5012bb4c730e5f100123ca144a04e3e5acc2357
Just Peter <ninh.peterrock@gmail.com>
2025-06-21 20:00:00 +0700

    update question

--- src/App.js
+++ src/App.js
@@ -278,6 +278,181 @@ const initialSampleQuestions = [
     id: 44,
     questionText: "44. H. Ân xá là gì?",
     correctAnswerText: "Thưa. Ân xá là việc tha thứ những hình phạt tạm đáng chịu vì những tội chúng ta đã phạm, dù những tội này đã được tha thứ."
+  },
+  {
+    id: 45,
+    questionText: "45. H. Bí Tích Xức Dầu Bệnh Nhân là gì?",
+    correctAnswerText: "T. Là bí tích Chúa Giê su đã lập, để ban ơn nâng đỡ bệnh nhân và người già yếu, về phần hồn cũng như phần xác."
+  },
+  {
+    id: 46,
+    questionText: "46. H. Ai có thể lãnh Bí Tích Xức Dầu?",
+    correctAnswerText: "T. Mọi tín hữu khi lâm bệnh nặng, khi gặp nguy tử hoặc bị yếu liệt vì tuổi già, đều có thể lãnh Bí Tích Xức Dầu."
+  },
+  {
+    id: 47,
+    questionText: "47. H. Bí Tích Xức Dầu được cử hành thế nào?",
+    correctAnswerText: "T. Bí Tích Xức Dầu được cử hành qua việc xức dầu trên trán và hai lòng bàn tay, với lời nguyện xin ân sủng của bí tích này."
+  },
+  {
+    id: 48,
+    questionText: "48. H. Bí Tích Xức Dầu có những hiệu quả nào?",
+    correctAnswerText: "T. Bí Tích Xức Dầu có những hiệu quả này:- Một là kết hợp bệnh nhân với cuộc khổ nạn của Chúa Kitô, để sinh ích cho họ và cho Hội Thánh;- Hai là mang lại cho họ niềm an ủi và lòng can đảm, để chịu đựng những đau đớn của bệnh tật hoặc tuổi già;- Ba là tha thứ các tội lỗi đã phạm nếu chưa xưng được;- Bốn là chữa lành thân xác nếu phù hợp với ý Chúa;- Năm là chuẩn bị cho “cuộc vượt qua” sang cõi sống đời đời."
+  },
+  {
+    id: 49,
+    questionText: "49. H. Bí Tích Truyền Chức Thánh là gì?",
+    correctAnswerText: "T. Là bí tích Chúa Giê su đã lập, để ủy thác sứ vụ của Ngài cho các Tông đồ, nhờ đó sứ vụ này tiếp tục thực thi trong Hội Thánh cho đến ngày tận thế."
+  },
+  {
+    id: 50,
+    questionText: "50. H. Chúa Giê su lập Bí Tích Truyền Chức Thánh khi nào?",
+    correctAnswerText: "T. Chúa Giê su lập Bí Tích Truyền Chức Thánh trong Bữa Tiệc Ly, khi Ngài nói với các Tông đồ rằng: “Anh em hãy làm việc này mà tưởng nhớ đến Thầy”."
+  },
+  {
+    id: 51,
+    questionText: "51. H. Bí Tích Truyền Chức Thánh có những hiệu quả nào?",
+    correctAnswerText: "T. Bí Tích Truyền Chức Thánh có những hiệu quả này:- Một là ban tràn đầy Chúa Thánh Thần;- Hai là làm cho người thụ phong nên đồng hình đồng dạng với Đức Kitô trong ba chức năng tư tế, tiên tri và vương đế theo từng cấp bậc của Bí Tích Truyền Chức Thánh;- Ba là trao ban một ấn tín thiêng liêng không tẩy xóa được."
+  },
+  {
+    id: 52,
+    questionText: "52. H. Bí Tích Truyền Chức Thánh gồm những cấp bậc nào?",
+    correctAnswerText: "T. Bí Tích Truyền Chức Thánh gồm ba cấp bậc này: một là phó tế, hai là linh mục, ba là giám mục."
+  },
+  {
+    id: 53,
+    questionText: "53. H. Những người lãnh Bí Tích Truyền Chức Thánh thi hành tác vụ với thẩm quyền nào?",
+    correctAnswerText: "T. Những người lãnh Bí Tích Truyền Chức Thánh thi hành tác vụ với thẩm quyền không do cộng đoàn ủy thác, nhưng do Đức Kitô trao ban. Họ thi hành tác vụ trong cương vị Đức Kitô là Đầu và nhân danh Hội Thánh. Vì thế, chức tư tế thừa tác khác biệt với chức tư tế cộng đồng mà các tín hữu đã lãnh nhận khi được rửa tội."
+  },
+  {
+    id: 54,
+    questionText: "54. H. Người tín hữu có bổn phận nào đối với các vị Chủ chăn của mình?",
+    correctAnswerText: "T. Người tín hữu có bổn phận cầu nguyện, tôn trọng, vâng lời và cộng tác với các vị Chủ chăn của mình trong việc xây dựng Hội Thánh, đồng thời giúp đỡ các ngài về cả tinh thần lẫn vật chất."
+  },
+  {
+    id: 55,
+    questionText: "55. H. Bí Tích Hôn Phối là gì?",
+    correctAnswerText: "T. Là bí tích Chúa Giê su đã lập, để kết hợp hai người tín hữu một nam một nữ thành vợ chồng trước mặt Thiên Chúa và Hội Thánh, cùng ban ân sủng để họ yêu thương nhau như Ngài đã yêu thương Hội Thánh."
+  },
+  {
+    id: 56,
+    questionText: "56. H. Hôn nhân Công giáo có mục đích nào?",
+    correctAnswerText: "T. Hôn nhân Công giáo có hai mục đích này:- Một là trọn đời yêu thương nhau;- Hai là sinh sản và dưỡng dục con cái."
+  },
+  {
+    id: 57,
+    questionText: "57. H. Bí Tích Hôn Phối được cử hành thế nào?",
+    correctAnswerText: "T. Bí Tích Hôn Phối được cử hành cách công khai, trước sự chứng kiến của vị đại diện Hội Thánh và những người làm chứng."
+  },
+  {
+    id: 58,
+    questionText: "58. H. Muốn lãnh Bí Tích Hôn Phối phải có những điều kiện nào?",
+    correctAnswerText: "T. Muốn lãnh Bí Tích Hôn Phối phải có những điều kiện này:- Một là đã lãnh nhận Bí Tích Rửa Tội;- Hai là không bị ngăn trở bởi luật tự nhiên và luật Hội Thánh;- Ba là hiểu biết về Bí Tích Hôn Phối và đời sống gia đình;- Bốn là có tự do kết hôn và công khai nói lên sự ưng thuận của mình theo nghi thức Hội Thánh."
+  },
+  {
+    id: 59,
+    questionText: "59. H. Hiệu quả của Bí Tích Hôn Phối là gì?",
+    correctAnswerText: "T. Bí Tích Hôn Phối tạo nên mối dây liên kết vĩnh viễn và độc chiếm giữa hai người phối ngẫu. Vì thế hôn nhân thành sự và hoàn hợp giữa những người đã được rửa tội không bao giờ có thể tháo gỡ được. Bí Tích Hôn Phối còn ban ân sủng cần thiết để họ đạt tới sự thánh thiện trong đời sống hôn nhân."
+  },
+  {
+    id: 60,
+    questionText: "60. H. Chủ đề mục vụ năm 2025 của Tổng Giáo phận Hà Nội là gì?",
+    correctAnswerText: "T. Chủ đề mục vụ năm 2025 của Tổng Giáo phận Hà Nội là “Canh tân đời sống đức tin trong cử hành phụng vụ.”"
+  },
+  {
+    id: 61,
+    questionText: "61. H. Canh tân đời sống đức tin trong cử hành phụng vụ là canh tân điều gì?",
+    correctAnswerText: "T. Canh tân đời sống đức tin trong cử hành phụng vụ là làm mới lại lòng yêu mến, tinh thần tham dự và cung cách cử hành phụng vụ, qua đó khám phá lại, bảo toàn và sống trọn vẹn những ý nghĩa xác thực và sức mạnh của các cử hành phụng vụ."
+  },
+  {
+    id: 62,
+    questionText: "62. H. Tại sao cần phải canh tân đời sống đức tin trong cử hành phụng vụ?",
+    correctAnswerText: "T. Phải canh tân đời sống đức tin trong cử hành phụng vụ vì những lý do này: - Thứ nhất vì phụng vụ là những mầu nhiệm thánh, là nguồn mạch trước tiên và thiết yếu để các tín hữu được thánh hoá, được hiệp thông mật thiết với Thiên Chúa và với nhau. - Thứ hai vì Hội Thánh tha thiết mong ước toàn thể các tín hữu được hướng dẫn tham dự cử hành phụng vụ cách trọn vẹn, ý thức và tích cực. - Thứ ba vì cần loại bỏ đi những cử hành cẩu thả, những lạm dụng vì thiếu hiểu biết, và thái độ tham dự khô khan nguội lạnh của nhiều tín hữu."
+  },
+  {
+    id: 63,
+    questionText: "63. H. Những ai được mời gọi phải canh tân đời sống đức tin trong cử hành phụng vụ?",
+    correctAnswerText: "T. Toàn thể các tín hữu, những người đã chịu Phép Rửa Tội, đã trở nên dòng dõi ưu tuyển, hàng tư tế vương giả, dân tộc thánh thiện, và đoàn dân được tuyển chọn (x. 1 Pr 2,9), được mời gọi phải không ngừng canh tân chính mình, để tham dự và cử hành phụng vụ cách trọn vẹn, ý thức và hiệu quả."
+  },
+  {
+    id: 64,
+    questionText: "64. H. Các thừa tác viên chức thánh cần làm gì để canh tân đời sống đức tin trong cử hành phụng vụ?",
+    correctAnswerText: "T. Các thừa tác viên chức thánh, nhất là các mục tử chăn dắt các linh hồn, cần phải làm những điều này: - Một là thấm nhuần sâu đậm tinh thần và năng lực của phụng vụ, và trở nên thầy dạy trong lãnh vực này. - Hai là luôn cử hành cách sốt sắng và trung thành các mầu nhiệm của Đức Kitô, đặc biệt là Hy tế Thánh Thể theo đúng tinh thần và luật phụng vụ của Hội Thánh. - Ba là chú trọng và kiên tâm thực hiện việc giảng dạy về phụng vụ cho các tín hữu, giúp họ tham dự phụng vụ tích cực cả bề trong lẫn bề ngoài."
+  },
+  {
+    id: 65,
+    questionText: "65. H. Các tín hữu giáo dân cần phải làm gì để canh tân đời sống đức tin trong cử hành phụng vụ?",
+    correctAnswerText: "T. Các tín hữu giáo dân cần phải thực hiện những điều này: - Một là siêng năng tham dự phụng vụ, nhất là tham dự Thánh lễ cách trọn vẹn, ý thức và hiệu quả. - Hai là chăm chỉ học hỏi về phụng vụ và trong phụng vụ. - Ba là dập khuôn đời sống đức tin theo phụng vụ, sống tình bác ái và dấn thân loan báo Tin Mừng."
+  },
+  {
+    id: 66,
+    questionText: "66. H. Làm thế nào để tham dự phụng vụ cách đích thực và trọn vẹn?",
+    correctAnswerText: "T. Tham dự phụng vụ cách đích thực và trọn vẹn là thực hiện những điều này: - Một là đến tham dự phụng vụ với thái độ sẵn sàng, chủ động, và hoà hợp cộng tác với ân sủng của Mầu nhiệm Thánh. - Hai là tham dự toàn vẹn cả tâm hồn và thể xác qua các cử chỉ, tư thế, hành động, lời đối đáp tung hô ca hát, và với sự thinh lặng cần thiết. - Ba là chăm chú lắng nghe Lời Chúa, tập dâng hiến chính mình hợp với Hy lễ Chúa Kitô, rước lễ và sống hiệp thông trọn vẹn với Thiên Chúa và với nhau. - Bốn là tham dự vào sứ vụ bác ái và truyền giáo sau cử hành phụng vụ."
+  },
+  {
+    id: 67,
+    questionText: "67. H. Các thừa tác viên như giúp lễ, đọc sách, dẫn lễ, và các ca viên cần phải làm gì để tham dự phụng vụ được trọn vẹn?",
+    correctAnswerText: "T. Các thừa tác viên như giúp lễ, đọc sách, dẫn lễ, ca viên là đang thực hiện một thừa tác vụ đích thực trong phụng vụ. Để được trọn vẹn, họ cần thực hiện những điều này: - Một là thi hành trọn vẹn và chỉ thi hành những gì thuộc lãnh vực mình tuỳ theo bản chất sự việc và những quy tắc phụng vụ. - Hai là thi hành phận vụ với lòng đạo đức chân thành và nghiêm túc. - Ba là phải thấm nhuần tinh thần phụng vụ, chăm chỉ học hỏi và rèn luyện để chu toàn các phần việc của mình theo đúng nghi thức và quy định."
+  },
+  {
+    id: 68,
+    questionText: "68. H. Để tăng thêm sự hiểu biết và yêu mến phụng vụ, người tín hữu cần làm những gì?",
+    correctAnswerText: "T. Người tín hữu cần làm những điều này: - Một là tham dự phụng vụ với thái độ ngỡ ngàng thán phục trước các mầu nhiệm được cử hành. - Hai là đào sâu khám phá vẻ đẹp của phụng vụ, nhất là ý nghĩa của ngôn ngữ biểu tượng trong phụng vụ. - Ba là chú tâm đến nghệ thuật cử hành, đến vẻ đẹp của chân lý, đến tính năng động và cách thức Chúa Thánh Thần tác động trong mỗi cử hành phụng vụ."
+  },
+  {
+    id: 69,
+    questionText: "69. H. Để nuôi dưỡng ý thức về sự thánh thiêng và cảm nhận nét đẹp của phụng vụ, cần phải loại bỏ những cung cách cử hành và thói quen tham dự phụng vụ nào?",
+    correctAnswerText: "T. Để nuôi dưỡng ý thức về sự thánh thiêng và cảm nhận nét đẹp của phụng vụ, cần loại bỏ những thiếu sót này: - Một là cử hành phụng vụ cách cẩu thả, thiếu sự chuẩn bị, có thái độ kiêu ngạo lấy mình làm trung tâm, hoặc không tuân theo những nguyên tắc của phụng vụ. - Hai là tham dự phụng vụ như những khách bàng quan và câm lặng, dửng dưng bên ngoài, sử dụng điện thoại, hoặc nói chuyện riêng trong giờ phụng vụ. - Ba là thái độ đi lễ chỉ vì sợ tội, sợ dư luận, hoặc chỉ để cầu lợi cầu may mà thôi."
+  },
+  {
+    id: 70,
+    questionText: "70. H. Năm Thánh là gì?",
+    correctAnswerText: "T. Năm Thánh hay còn gọi là năm Toàn Xá, là một thời kỳ hồng ân, qua đó Thiên Chúa ban ơn đặc biệt hơn khi con người mở lòng để thống hối và canh tân. Năm Thánh là thời gian giao hoà với Thiên Chúa và anh chị em, nhằm thánh hóa cuộc sống, củng cố đức tin, tạo cơ hội thuận tiện để xây dựng tình liên đới và hiệp thông huynh đệ trong lòng Giáo Hội và thế giới, khuyến khích mọi người tuyên xưng đức tin một cách chân thành và sống động hơn nơi Ðức Ki tô, Ðấng Cứu Ðộ duy nhất của nhân loại."
+  },
+  {
+    id: 71,
+    questionText: "71. H. Để mở Năm Thánh thường lệ 2025, Đức Thánh Cha Phan xi cô đã ban hành tông sắc gì?",
+    correctAnswerText: "T. Đức Thánh Cha Phan xi cô đã ban hành tông sắc “Spes non Confundit” nghĩa là “Hy vọng không làm thất vọng”."
+  },
+  {
+    id: 72,
+    questionText: "72. H. Năm Thánh thường lệ 2025 có khẩu hiệu là gì?",
+    correctAnswerText: "T. Năm Thánh 2025 có khẩu hiệu là “Những người hành hương của hy vọng”."
+  },
+  {
+    id: 73,
+    questionText: "73. H. Hành hương Năm Thánh mang những ý nghĩa gì?",
+    correctAnswerText: "T. Hành hương Năm Thánh có những ý nghĩa này: - Một là lên đường hành hương đến một nơi thánh và bước qua Cửa thánh. - Hai là sống kinh nghiệm hoán cải, biến đổi chính bản thân để phù hợp với sự thánh thiện của Thiên Chúa. - Ba là cuộc hành trình ra khỏi chính mình, bước theo Đức Kitô như người môn đệ, để từng bước nên đồng hình đồng dạng với Người."
+  },
+  {
+    id: 74,
+    questionText: "74. H. Hy vọng Kitô giáo có những đặc tính nào?",
+    correctAnswerText: "T. Hy vọng Kitô giáo có những đặc tính này: - Một là hy vọng phát sinh từ trái tim bị đâm thâu của Chúa Giê su trên thập giá. - Hai là hy vọng không lừa dối cũng không làm thất vọng. - Ba là hy vọng như cái neo chắc chắn và bền vững của tâm hồn, thả sâu vào nơi Chúa Giê su đã vào như người tiền phong mở đường cho chúng ta (x. Dt, chương 6, câu 19 đến câu 20; Giáo lý Hội Thánh Công Giáo số 1820). - Bốn là hy vọng đặt trên nền tảng đức tin và được nuôi dưỡng bằng đức ái, bằng tình yêu Thiên Chúa. - Năm là hy vọng hướng về cùng đích là hạnh phúc vĩnh cửu trong hiệp thông trọn vẹn với Thiên Chúa."
+  },
+  {
+    id: 75,
+    questionText: "75. H. Bởi đâu mà hy vọng Kitô giáo không lừa dối cũng không làm thất vọng?",
+    correctAnswerText: "T. Hy vọng Kitô giáo không lừa dối cũng không làm thất vọng vì nó dựa trên tình yêu vô điều kiện mà Thiên Chúa đã đổ vào lòng chúng ta, nhờ Thánh Thần mà Người ban cho chúng ta (x. Rô ma chương 5, câu 5), giúp chúng ta xác tín rằng không có gì và không ai có thể tách chúng ta ra khỏi tình yêu của Thiên Chúa (x. Rô ma chương 8, câu 35 đến câu 39)."
+  },
+  {
+    id: 76,
+    questionText: "76. H. Người tín hữu cần làm gì để trở nên người hành hương của hy vọng?",
+    correctAnswerText: "T. Để trở nên người hành hương của hy vọng, người tín hữu làm những điều này: - Một là kín múc niềm hy vọng nơi ân sủng của Thiên Chúa qua cuộc gặp gỡ sống động thân tình với Đức Ki tô, như đi hành hương, sống kiên nhẫn, ẩn náu nơi Thiên Chúa qua cầu nguyện và cử hành phụng vụ, nhất là qua bí tích hòa giải và lãnh nhận ân xá. - Hai là khám phá lại niềm hy vọng qua những dấu chỉ của thời đại, nhất là lưu tâm tới những điều thiện hảo trên thế giới. - Ba là sống chứng nhân và trở nên dấu chỉ của hy vọng cho con người trong thế giới hôm nay."
+  },
+  {
+    id: 77,
+    questionText: "77. H. Đức Thánh Cha Phan xi cô gợi mở những gì cần thực hiện để là dấu chỉ hy vọng cho thế giới hôm nay?",
+    correctAnswerText: "T. Để là dấu chỉ hy vọng cho thế giới hôm nay, Đức Thánh Cha Phan xi cô đã gợi mở những điều này: - Một là dấn thân xây dựng hòa bình trên thế giới. - Hai là có lòng nhiệt thành với cuộc sống và sẵn sàng chia sẻ cuộc sống qua việc sinh thêm nhiều con cái và tìm lại niềm vui sống vì được tạo dựng theo hình ảnh Thiên Chúa. - Ba là biểu lộ sự gần gũi với các tù nhân, kêu gọi ân xá và giảm án, bãi bỏ án tử hình và giúp tù nhân tái hòa nhập cộng đồng. - Bốn là xoa dịu những đau khổ cho bệnh nhân bằng cách thăm nom trong tình yêu thương và sự gần gũi. - Năm là truyền cảm hứng cho giới trẻ, gần gũi và đồng hành với họ. - Sáu là mở rộng vòng tay đón nhận người di cư và tị nạn, sẵn sàng bảo vệ quyền lợi của những người yếu thế nhất. - Bảy là trân trọng và nâng đỡ người cao tuổi. - Tám là mang lại niềm hy vọng cho người nghèo, lập quỹ toàn cầu để triệt để xóa bỏ nạn đói, giúp cho nước nghèo phát triển, xóa nợ và tôn trọng môi sinh."
+  },
+  {
+    id: 78,
+    questionText: "78. H. Trở nên dấu chỉ hy vọng là trách nhiệm và bổn phận của ai?",
+    correctAnswerText: "T. Tất cả những người đã được Rửa tội, mỗi người với đặc sủng và nhiệm vụ của mình, đều có trách nhiệm trở nên những dấu chỉ hy vọng minh chứng cho sự hiện diện của Thiên Chúa giữa lòng thế giới."
+  },
+  {
+    id: 79,
+    questionText: "79. H. Người tín hữu chứng minh về đức tin và đức mến trong lòng mình bằng cách nào?",
+    correctAnswerText: "T. Người tín hữu chứng minh về đức tin và đức mến trong lòng mình bằng cách sống tràn đầy niềm hy vọng, để có thể trao đi dù chỉ là một nụ cười, một cử chỉ thân tình, một cái nhìn huynh đệ, một sự lắng nghe chân thành, một sự phục vụ vô vị lợi, vì biết rằng, trong Thần Khí của Chúa Giê su, điều này có thể trở thành hạt giống trổ sinh hy vọng nơi những ai đón nhận."
   }
 ];
 
